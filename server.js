e0febaf5774dfe296b3fe779503d77b06a2ba003
Andy-William <frozen_altar@yahoo.co.id>
2018-05-06 19:45:22 +0700

    punctuation

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ bot.on('text', (msg) => {
   );
 
   if( wrongs.length ) {
-    const warningMsg = "YA AMPUN! Yang bener itu " + wrongs.join(', ') + " buka KBBI sana!";
+    const warningMsg = "YA AMPUN! Yang bener itu " + wrongs.join(', ') + ". Buka KBBI sana!";
     msg.reply.text(warningMsg, { asReply: true });
   }
 });
