2392c538e7a2cfa86eed4d6a494c3447201ae8ef
shidqon <shidqon.famulaqih@bukalapak.com>
2018-05-06 15:31:24 +0700

    use regex

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const bot = new TeleBot('542872609:AAExPeniw6xESTMptH-feb0mynw0dKIar00');
 
 bot.on(['/start', '/hello'], (msg) => msg.reply.text('Selamat datang! Silakan dicoba!'));
 bot.on('text', (msg) => {
-  const hasSilahkan = msg.text.indexOf('silahkan') !== -1;
-  if (hasSilahkan) {
+  const regex = /silahkan/;
+  const warning = regex.test(msg.text);
+  if (warning) {
     const warningMsg = "YA AMPUN! Yang bener itu 'silakan' buka KBBI sana!";
     msg.reply.text(warningMsg);
   }
