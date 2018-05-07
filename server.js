27c45c86138e2eea8304564a93757ea676e29f6f
Andy-William <frozen_altar@yahoo.co.id>
2018-05-07 20:22:26 +0700

    fullowrd check, hyperlink, more vocab

--- server.js
+++ server.js
@@ -11,22 +11,37 @@ const wordList = require('read-yaml').sync('wordlist.yml');
 console.log(wordList)
 
 bot.on('text', (msg) => {
-  console.log(msg.from.username + ': ' + msg.text)
+  console.log('(' + msg.chat.type + ' - ' + msg.chat.title + ') ' + msg.from.username + ': ' + msg.text)
+  if( msg.text.match(new RegExp('(shit|n(u|oo)b) bot','i')) ) {
+    msg.reply.text('no u', {asReply: true});
+    return;
+  }
 
   wrongs = []
 
+  // substring check
   Object.entries(wordList.substring).forEach(
-    ([wrong, correct]) => {
+    ([correct, wrong]) => {
       if( msg.text.match(new RegExp(wrong,'i')) ){
         wrongs.push(correct)
       }
     }
   );
 
+  // fullword check
+  Object.entries(wordList.fullword).forEach(
+    ([correct, wrong]) => {
+      if( msg.text.match(new RegExp('([^a-zA-Z]|^)' + wrong + '([^a-zA-Z]|$)','i')) ){
+        wrongs.push(correct)
+      }
+    }
+  );
+
   if( wrongs.length ) {
-    const warningMsg = "YA AMPUN! Yang bener itu " + wrongs.join(', ') + ". Buka KBBI sana!";
-    msg.reply.text(warningMsg, { asReply: true });
+    const warningMsg = "YA AMPUN! Yang bener itu *" + wrongs.join('*, *') + "*. Buka [KBBI](https://www.kbbi.web.id/" + wrongs[0] + ") sana!";
+    msg.reply.text(warningMsg, { asReply: true, parseMode: 'markdown', webPreview: false });
   }
+
 });
 
 bot.start();
